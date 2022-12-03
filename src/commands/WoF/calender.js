897b4f4728c11b90732eacc3d631fb2bae90cf25
Paul Koplin <paul.koplin@web.de>
2022-12-03 00:00:30 +0000

    fix calnder

--- src/commands/WoF/calender.js
+++ src/commands/WoF/calender.js
@@ -34,15 +34,7 @@ module.exports = class CalenderBreakSlashCommand extends BaseSlashCommand {
     }
 
     getRandomId(min, max, limitId, wheel) {
-        let id = Math.floor(Math.random() * (max - min + 1) + min);
-
-        for (let i = 0; i < wheel[id].limit.length; i++) {
-            if (limitId === wheel[id].limit[i]) {
-                id = this.getRandomId(min, max, limitId, wheel)
-            }
-        }
-
-        return id;
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
     setOption(text) {
